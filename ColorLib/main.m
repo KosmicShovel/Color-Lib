6656fea410bf10a25a80c47417043e0862958f54
Matthew Guerra <2matthew05@gmail.com>
2021-04-10 15:21:16 -0500

    Initial Commit

---
+++ ColorLib/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorLib
+//
+//  Created by Matthew Guerra on 4/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
